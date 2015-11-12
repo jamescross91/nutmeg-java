145d94ea193de0a6973d309d784ebed4d5160a95
jamescross91 <james_cross91@hotmail.com>
2015-11-12 16:29:10 +0000

    Adding maven build plugin, argument parsing

--- src/main/java/io/jamescross/Main.java
+++ src/main/java/io/jamescross/Main.java
@@ -1,6 +1,9 @@
 package io.jamescross;
 
+import io.jamescross.model.Fact;
 import io.jamescross.rest.bytes.ImageConsumer;
+import io.jamescross.rest.json.FactConsumer;
+import io.jamescross.rest.xml.CategoryConsumer;
 
 /**
  * Created by jamescross91 on 12/11/2015.
@@ -8,16 +11,32 @@ import io.jamescross.rest.bytes.ImageConsumer;
 
 public class Main {
     public static void main(String args[]) {
-//        try {
-//            Fact fact = new FactConsumer().consumeResource("http://catfacts-api.appspot.com/api/facts");
-//            System.out.println(fact.getFacts());
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (UnirestException e) {
-//            e.printStackTrace();
-//        }
+        if(args.length != 1) {
+            System.out.println("Expecting exactly 1 argument: file, categories or fact.  Defaulting to file.");
+            fileOption();
+        } else {
+            switch(args[1]) {
+                case "file": fileOption();
+                    break;
+                case "categories": categoryOption();
+                    break;
+                case "fact": factOption();
+                    break;
+                default: System.out.printf("Invalid argument: please use either file, categories, or fact.");
+            }
+        }
+    }
 
-        //new CategoryConsumer().consumeResource("http://thecatapi.com/api/categories/list");
+    private static void fileOption() {
         System.out.println(new ImageConsumer().consumeResource("http://thecatapi.com/api/images/get"));
+    }
+
+    private static void categoryOption() {
+        System.out.println(new CategoryConsumer().consumeResource("http://thecatapi.com/api/categories/list"));
+    }
+
+    private static void factOption() {
+        Fact fact = new FactConsumer().consumeResource("http://catfacts-api.appspot.com/api/facts");
+        System.out.println(fact.getFacts());
     }
 }
